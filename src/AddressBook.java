6fb0619e182e94603a6ed14948f76b4b46e7c541
alialvi00 <ali_mashhood98@hotmail.com>
2020-09-24 17:42:40 -0400

    Print statement + Testing methods

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -9,15 +9,11 @@ public class AddressBook {
     BuddyInfo buddy4 = new BuddyInfo("Michael","Scarborough","416");
     BuddyInfo buddy5 = new BuddyInfo("John","Mississauga","905");
 
-    public AddressBook(){
-        buddyColl.add(buddy2);
-        buddyColl.add(buddy3);
-        buddyColl.add(buddy4);
-        buddyColl.add(buddy5);
-    }
-
     public static void main(String[] args){
         System.out.println("Address Book");
+        AddressBook addy = new AddressBook();
+        addy.addBuddy(addy.buddy2);
+        addy.removeBuddy(addy.buddy2);
     }
 
     public void addBuddy(BuddyInfo newBuddy){
